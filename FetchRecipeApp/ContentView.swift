b77f5607c983b7bd1419e6b590c49c27a1d64c7c
Raul Pham <phatpham314@icloud.com>
2023-02-27 20:05:06 -0600

    Initial Commit

---
+++ FetchRecipeApp/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  FetchRecipeApp
+//
+//  Created by Pham on 2/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+          NavigationView {
+                List {
+                      
+                }
+                .navigationTitle("List")
+                .onAppear {
+                      
+                }
+          }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FetchRecipeApp/FetchRecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipeAppApp.swift
+//  FetchRecipeApp
+//
+//  Created by Pham on 2/27/23.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
