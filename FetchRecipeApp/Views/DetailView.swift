6ab4b91a16060b871ab6f13ae4b429a82e4b9cb0
Raul Pham <phatpham314@icloud.com>
2023-03-01 00:10:18 -0600

    v0.0.7 - update home view - update detail view

--- FetchRecipeApp/Views/DetailView.swift
+++ FetchRecipeApp/Views/DetailView.swift
@@ -11,18 +11,57 @@ struct DetailView: View {
       @ObservedObject var mealDetailModel =  MealDetailModel()
       let mealId: String
       
-    var body: some View {
-          VStack {
-                ForEach(mealDetailModel.dessertDetails.meals, id: \.self) { index in
-                      Text(index.strMeal)
-                }
-          }
-          .onAppear {
-                mealDetailModel.getDessertDetail(detailId: mealId)
-          }
-    }
-      
-      
+      var body: some View {
+            ScrollView {
+                  GeometryReader { geo in
+                        VStack(alignment: .leading) {
+                              ForEach(mealDetailModel.dessertDetails.meals, id: \.idMeal) { index in
+                                    VStack(alignment: .leading) {
+                                          ZStack {
+                                                Rectangle()
+                                                      .foregroundColor(.white)
+                                                      .frame(width: geo.size.width, height: geo.size.height + 200, alignment: .center)
+                                                      .cornerRadius(5)
+                                                      .shadow(color: Color(.sRGB, red: 0, green: 0, blue: 0, opacity: 0.5), radius: 10, x: -5, y: 6)
+                                                
+                                                AsyncImage(url: URL(string: String(index.strMealThumb))) { image in
+                                                      image
+                                                            .resizable()
+                                                            .scaledToFill()
+                                                            .frame(width: geo.size.width, height: geo.size.height + 200, alignment: .center)
+                                                            .clipped()
+                                                            .cornerRadius(5)
+                                                } placeholder: {
+                                                      ZStack {
+                                                            Rectangle()
+                                                                  .foregroundColor(Color(UIColor.systemGray6))
+                                                                  .frame(width: geo.size.width, height: geo.size.height + 200, alignment: .center)
+                                                                  .cornerRadius(5)
+                                                            
+                                                            VStack {
+                                                                  ProgressView(value: 1.0)
+                                                                        .progressViewStyle(CircularProgressViewStyle())
+                                                                  
+                                                                  Text("Image Loading")
+                                                                        .foregroundColor(.black)
+                                                                        .font(Font.custom("Avenir Heavy", size: 6))
+                                                            }
+                                                      }
+                                                }
+                                          }
+                                          
+                                          Text(index.strCategory)
+                                    }
+                                    .navigationTitle(index.strMeal)
+                              }
+                        }
+                  }
+                  .padding([.leading, .trailing])
+            }
+            .onAppear {
+                  mealDetailModel.getDessertDetail(detailId: mealId)
+            }
+      }
 }
 
 struct DetailView_Previews: PreviewProvider {
--- FetchRecipeApp/Views/HomeView.swift
+++ FetchRecipeApp/Views/HomeView.swift
@@ -13,12 +13,56 @@ struct HomeView: View {
       
     var body: some View {
           NavigationView {
-                ScrollView {
-                        ForEach(mealModel.dessert.meals, id: \.idMeal) { index in
-                              NavigationLink(destination: DetailView(mealId: index.idMeal)) {
-                                    Text(index.strMeal)
-                              }
-                        }
+                VStack(alignment: .leading) {
+                      Text("All Recipes")
+                            .bold()
+                            .font(Font.custom("Avenir Heavy", size: 24))
+                            .padding(.leading)
+                      
+                      ScrollView {
+                            LazyVStack(alignment: .leading) {
+                                  ForEach(mealModel.dessert.meals, id: \.idMeal) { index in
+                                        NavigationLink(
+                                          destination: DetailView(mealId: index.idMeal),
+                                          label: {
+                                                HStack(spacing: 20.0) {
+                                                      AsyncImage(url: URL(string: String(index.strMealThumb))) { image in
+                                                            image
+                                                                  .resizable()
+                                                                  .scaledToFill()
+                                                                  .frame(width: 50, height: 50, alignment: .center)
+                                                                  .clipped()
+                                                                  .cornerRadius(5)
+                                                                  .shadow(color: Color(.sRGB, red: 0, green: 0, blue: 0, opacity: 0.5), radius: 10, x: -5, y: 6)
+                                                      } placeholder: {
+                                                            ZStack {
+                                                                  Rectangle()
+                                                                        .foregroundColor(Color(UIColor.systemGray6))
+                                                                        .frame(width: 50, height: 50, alignment: .center)
+                                                                        .cornerRadius(5)
+                                                                        .shadow(color: Color(.sRGB, red: 0, green: 0, blue: 0, opacity: 0.5), radius: 10, x: -5, y: 6)
+                                                                  
+                                                                  VStack {
+                                                                        ProgressView(value: 1.0)
+                                                                              .progressViewStyle(CircularProgressViewStyle())
+                                                                        
+                                                                        Text("Image Loading")
+                                                                              .foregroundColor(.black)
+                                                                              .font(Font.custom("Avenir Heavy", size: 6))
+                                                                  }
+                                                            }
+                                                      }
+                                                      VStack(alignment: .leading) {
+                                                            Text(index.strMeal)
+                                                                  .foregroundColor(.black)
+                                                                  .font(Font.custom("Avenir Heavy", size: 16))
+                                                      }
+                                                }
+                                          })
+                                  }
+                            }
+                            .padding(.leading)
+                      }
                 }
           }
     }
@@ -27,5 +71,6 @@ struct HomeView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+                .environmentObject(MealModel())
     }
 }
